be3bd28c1d28ecabe533c8b371185d98d3d76660
Vibha Maurya <vibhamaurya060@gmail.com>
2025-02-09 14:06:03 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 const jsonServer=require("json-server");
+const cors=require("cors");
 const server=jsonServer.create();
 const router=jsonServer.router("db.json");
 const middlewares=jsonServer.defaults();
 const port=process.env.PORT || 3000;
 
+const frontendUrl = "https://design-daredevils-052-3fnc.vercel.app"; 
+
+
+server.use(
+  cors({
+    origin: frontendUrl, 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  })
+);
+
 server.use(middlewares);
 server.use(router);
-server.listen(port);
-console.log("server is running on Port:", port)
+server.listen(port, ()=>{
+    console.log("server is running on Port:", port)
+});
